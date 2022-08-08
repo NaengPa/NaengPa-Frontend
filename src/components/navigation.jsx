a2a2587820ae916abcecb440c1657e7a187d09dd
Sangjo LEE <cent7425@gmail.com>
2022-08-09 04:46:07 +0900

    feat: :sparkles: 하단 네비게이션 아이콘 현재 페이지에 대응하여 색칠되도록 수정 (#7)

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ReactComponent as Home } from "../assets/icon_home.svg";
@@ -6,24 +6,48 @@ import { ReactComponent as community } from "../assets/icon_community.svg";
 import { ReactComponent as mypage } from "../assets/icon_mypage.svg";
 import { ReactComponent as refrigerator } from "../assets/icon_refrigerator.svg";
 import { ReactComponent as search } from "../assets/icon_search.svg";
+import { css } from "styled-components";
 
 const Navigation = () => {
+  const [, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+
   return (
     <Container>
-      <Link to={"/"}>
-        <StyledMyIconHome></StyledMyIconHome>
+      <Link to={{ pathname: "/" }} onClick={forceUpdate}>
+        <StyledMyIconHome
+          isActive={window.location.pathname === "/" ? true : false}
+        />
       </Link>
-      <Link to={"/search"}>
-        <StyledMyIconSearch></StyledMyIconSearch>
+      <Link to={{ pathname: "/search" }} onClick={forceUpdate}>
+        <StyledMyIconSearch
+          isActive={
+            window.location.pathname === "/search" ||
+            window.location.pathname.indexOf("/detail") !== -1
+              ? true
+              : false
+          }
+        />
       </Link>
-      <Link to={"/myfrige"}>
-        <StyledMyIconRefrigerator></StyledMyIconRefrigerator>
+      <Link to={{ pathname: "/myfrige" }} onClick={forceUpdate}>
+        <StyledMyIconRefrigerator
+          isActive={
+            window.location.pathname === "/myfrige" ||
+            window.location.pathname === "/frige"
+              ? true
+              : false
+          }
+        />
       </Link>
-      <Link to={"/community"}>
-        <StyledMyIconCommunity></StyledMyIconCommunity>
+      <Link to={{ pathname: "/community" }} onClick={forceUpdate}>
+        <StyledMyIconCommunity
+          isActive={window.location.pathname === "/community" ? true : false}
+        />
       </Link>
-      <Link to={"/mypage"}>
-        <StyledMyIconMypage></StyledMyIconMypage>
+      <Link to={{ pathname: "/mypage" }} onClick={forceUpdate}>
+        <StyledMyIconMypage
+          isActive={window.location.pathname === "/mypage" ? true : false}
+        />
       </Link>
     </Container>
   );
@@ -46,8 +70,48 @@ const Container = styled.div`
   max-width: 420px;
 `;
 
-const StyledMyIconHome = styled(Home)``;
-const StyledMyIconCommunity = styled(community)``;
-const StyledMyIconMypage = styled(mypage)``;
-const StyledMyIconRefrigerator = styled(refrigerator)``;
-const StyledMyIconSearch = styled(search)``;
+const StyledMyIconHome = styled(Home)`
+  ${(props) =>
+    props.isActive &&
+    css`
+      path {
+        stroke: #2e8cfe;
+      }
+    `}
+`;
+const StyledMyIconCommunity = styled(community)`
+  ${(props) =>
+    props.isActive &&
+    css`
+      path {
+        stroke: #2e8cfe;
+      }
+    `}
+`;
+const StyledMyIconMypage = styled(mypage)`
+  ${(props) =>
+    props.isActive &&
+    css`
+      path {
+        stroke: #2e8cfe;
+      }
+    `}
+`;
+const StyledMyIconRefrigerator = styled(refrigerator)`
+  ${(props) =>
+    props.isActive &&
+    css`
+      path {
+        stroke: #2e8cfe;
+      }
+    `}
+`;
+const StyledMyIconSearch = styled(search)`
+  ${(props) =>
+    props.isActive &&
+    css`
+      path {
+        stroke: #2e8cfe;
+      }
+    `}
+`;
