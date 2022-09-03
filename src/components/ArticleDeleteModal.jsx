bf9089e427201d3200382a3ac8eefa8d304d1ab4
Sangjo LEE <cent7425@gmail.com>
2022-09-03 23:36:31 +0900

    fix: :bug: 모달과 nav의 z-index값 조정 (#56)

--- src/components/ArticleDeleteModal.jsx
+++ src/components/ArticleDeleteModal.jsx
@@ -50,7 +50,7 @@ const DeleteModalDimmer = styled.div`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   width: 100%;
   height: 100vh;
-  z-index: 101;
+  z-index: 100;
 `;
 
 const DeleteBtnContainer = styled.div`
