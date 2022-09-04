bcc43aa6876320775f42f7fec8125942e5625644
Sangjo LEE <cent7425@gmail.com>
2022-09-04 19:11:18 +0900

    feat: :bug: 레이아웃 높이 조절 (#65)

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const StyledMainLayout = styled.div`
   position: relative;
   max-width: 420px;
-  height: 100vh;
+  height: calc(100vh - 56px);
   margin: auto;
   overflow-y: hidden;
 `;
